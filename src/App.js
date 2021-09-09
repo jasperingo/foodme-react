964812ef70bb2f8514ecdbfd27f5a41e7ee9271e
Jasper Anelechukwu <jasperanels@gmail.com>
2021-09-09 19:35:50 +0100

    Initial modifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header" style={{ color: 'gold' }}>
-          Welcome to FOODME app
+    <>
+      <header className="App-header">
+          <h1>Welcome to FoodMe Resturants</h1>
+          <em>Under construction...</em>
       </header>
-    </div>
+    </>
   );
 }
 
