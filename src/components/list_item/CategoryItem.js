5082145b23e13fae0261abe2c5665cb9e28d9d32
Jasper Anelechukwu <jasperanels@gmail.com>
2022-02-22 17:17:19 +0100

    Modified

--- src/components/list_item/CategoryItem.js
+++ src/components/list_item/CategoryItem.js
@@ -13,10 +13,16 @@ export default function CategoryItem({ canEdit, category, index, sub, path, grid
     <li className="mb-2">
       <Link 
         to={canEdit ? `/sub-category/${category.id}/update` : fullPath} 
-        className={`${grid ? 'block text-center shadow' : 'flex'} flex-grow items-center gap-2 bg-color py-2 rounded md:px-2 md:py-3 md:shadow md:block md:text-center hover:bg-color-gray-h`}
+        className={`${grid ? 'block text-center shadow' : 'flex'} flex-grow items-center gap-2 bg-color p-2 rounded md:py-3 md:shadow md:block md:text-center hover:bg-color-gray-h`}
         >
-        <img src={category.photo.href} alt={category.name} width="100" height="100" className="w-10 h-10 block mx-auto rounded" />
-        <div className={`text-sm flex-grow truncate overflow-ellipsis ${iconColor}`}>{ category.name }</div>
+        <img 
+          src={category.photo.href} 
+          alt={category.name} 
+          width="100" 
+          height="100" 
+          className={`${grid ? 'w-full' : 'w-20'} h-20 block mx-auto rounded lg:h-52 lg:w-full lg:mb-1`}
+          />
+        <div className={`flex-grow truncate overflow-ellipsis ${iconColor}`}>{ category.name }</div>
       </Link>
     </li>
   );
